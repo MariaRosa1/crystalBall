f7edcef8936a2879452b331197c2c96f1100c8f4
Maria Clara da Rosa <106022405+MariaRosa1@users.noreply.github.com>
2022-06-13 12:41:39 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const possivelRespostas = [
+    "Com certeza!",
+    "Não tenho tanta certeza.",
+    "Não conte com isso.",
+    "Sem dúvidas!",
+    "Sim, definitivamente!",
+    "Minha resposta é não.",
+    "Você pode contar com isso.",
+    "Ao meu ver, sim.",
+    "Minhas fontes dizem não.",
+    "Provavelmente.",
+    "Sinais apontam que não.",
+    "Perspectiva boa.",
+    "Definitivamente, não.",
+    "Minhas fontes dizem sim.",
+    "As perspectivas não são tão boas.",
+    "Sim.",
+    "Concentre-se no objetivo que dará certo.",
+    "Sinais apontam que sim.",
+    "Não.",
+]
+
+function clicar(){
+    const pergunta = document.getElementById("pergunta").value;
+    const resposta = document.getElementById("resposta");
+    const resultado = document.getElementById("resultado");
+
+    if(pergunta.length == 0){
+        alert("Por favor, faça uma pergunta")
+        return
+    }
+
+    //number random
+    let numeroAleatorio = Math.floor(Math.random() * possivelRespostas.length)
+
+    //resposta
+    resposta.innerHTML += pergunta + "?"
+    resultado.innerHTML += possivelRespostas[numeroAleatorio];
+
+    setTimeout(function (){
+        resposta.innerHTML = ''
+        resultado.innerHTML = ''
+      }, "3000")
+}
+
---
+++ js/teste.js
@@ -0,0 +1,24 @@
+const possivelRespostas = [
+    "Com certeza!",
+    "Não tenho tanta certeza.",
+    "É decididamente assim.",
+    "Não conte com isso.",
+    "Sem dúvidas!",
+    "Sim, definitivamente!",
+    "Minha resposta é não.",
+    "Você pode contar com isso.",
+    "Melhor não te dizer agora.",
+    "Ao meu ver, sim.",
+    "Minhas fontes dizem não.",
+    "Provavelmente.",
+    "Sinais apontam que não.",
+    "Perspectiva boa.",
+    "As perspectivas não são tão boas.",
+    "Sim.",
+    "Concentre-se no objetuvo que dará certo.",
+    "Sinais apontam que sim.",
+    "Não.",
+  ]
+
+  let numeroAleatorio = Math.floor(Math.random() * possivelRespostas.value)
+  console.log(numeroAleatorio)
